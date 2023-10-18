2b900e3b42db610250beeec839836af809853bd3
ranitsarker <rntprince@gmail.com>
2023-10-18 23:13:18 +0600

    I have created product update endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -74,7 +74,14 @@ app.use(express.json());
         const microsoftProducts = await cursor.toArray();
         res.send(microsoftProducts);
       });
-      
+      // product update endpoint 
+      app.get('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollection.findOne(query);
+        res.send(result);
+      });
+
 
 
 
